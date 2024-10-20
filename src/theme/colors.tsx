3500c5cd65c05e6fa356814de40863e506c0ef2a
Artyom Ulanchik <artyom.u@outlook.com>
2024-10-20 22:20:20 +0100

    added colors

---
+++ src/theme/colors.tsx
@@ -0,0 +1,17 @@
+const colors = {
+  white: "#fff",
+  blueDark: "#0B98C3",
+  blueLight: "#22A8D1",
+  orange: "#ee7e33",
+  grey1: "#2F2F2F",
+  grey2: "#4D4D4D",
+  grey3: "#3D4043",
+  grey4: "#717171",
+  grey5: "#A1A1A1",
+  grey6: "#F5F5F5",
+  grey7: "#D3D3D3",
+  redDefault: "#ff0065",
+  redLight: "#ffe5ef",
+};
+
+export default colors;
