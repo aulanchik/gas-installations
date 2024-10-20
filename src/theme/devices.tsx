eb49557237c0a451a5ff18ece54359438177c3ae
Artyom Ulanchik <artyom.u@outlook.com>
2024-10-20 22:20:53 +0100

    added device screen and sizes

---
+++ src/theme/devices.tsx
@@ -0,0 +1,15 @@
+import sizes from "./sizes";
+
+export const devices = {
+  mobileS: `(max-width: ${sizes.mobileS})`,
+  mobileM: `(max-width: ${sizes.mobileM})`,
+  mobileL: `(max-width: ${sizes.mobileL})`,
+  tablet: `(max-width: ${sizes.tablet})`,
+  tabletL: `(max-width: ${sizes.tabletL})`,
+  laptop: `(max-width: ${sizes.laptop})`,
+  laptopL: `(max-width: ${sizes.laptopL})`,
+  desktop: `(max-width: ${sizes.desktop})`,
+  desktopL: `(max-width: ${sizes.desktop})`,
+};
+
+export default devices;
---
+++ src/theme/sizes.tsx
@@ -0,0 +1,12 @@
+const sizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "440px",
+  tablet: "768px",
+  tabletL: "950px",
+  laptop: "1200px",
+  laptopL: "1400px",
+  desktop: "2560px",
+};
+
+export default sizes;
