bce09faff229d826220062b6e33a6b4213e8a48e
Artyom Ulanchik <artyom.u@outlook.com>
2024-10-21 14:00:14 +0100

    adding missing webp exports

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,12 +4,12 @@ import iconHouseB from "./icons/houseB.svg?url";
 import iconBungalowW from "./icons/bungalowW.svg?url";
 import iconBungalowB from "./icons/bungalowB.svg?url";
 import iconBungalowG from "./icons/bungalowG.svg?url";
-import backgroundsBoilerLeft from "./backgrounds/boiler-left.png?url";
-import backgroundsBoilerRight from "./backgrounds/boiler-right.png?url";
-import backgroundsHero from "./backgrounds/hero.jpg?url";
-import backgroundsHandshake from "./backgrounds/handshake.jpeg?url";
-import backgroundsMobHero from "./backgrounds/mob-hero.jpg?url";
-import backgroundsMobHandshake from "./backgrounds/mob-handshake.jpeg?url";
+import bgBoilerLeft from "./backgrounds/boiler-left.png?url";
+import bgBoilerRight from "./backgrounds/boiler-right.png?url";
+import bgHero from "./backgrounds/hero.jpg?url";
+import bgHandshake from "./backgrounds/handshake.jpeg?url";
+import bgMobHero from "./backgrounds/mob-hero.jpg?url";
+import bgMobHandshake from "./backgrounds/mob-handshake.jpeg?url";
 import iconCombiW from "./icons/combiW.svg?url";
 import iconCombiB from "./icons/combiB.svg?url";
 import iconStandardW from "./icons/standardW.svg?url";
@@ -18,6 +18,12 @@ import iconSystemW from "./icons/systemW.svg?url";
 import iconSystemB from "./icons/systemB.svg?url";
 import iconBackBoilerW from "./icons/back-boilerW.svg?url";
 import iconBackBoilerB from "./icons/back-boilerB.svg?url";
+import webpBgBoilerLeft from "./backgrounds/webp/boiler-left.webp?url";
+import webpBgBoilerRight from "./backgrounds/webp/boiler-right.webp?url";
+import webpBgHero from "./backgrounds/webp/hero.webp?url";
+import webpBgMobHero from "./backgrounds/webp/mob-hero.webp?url";
+import webpBgHandshake from "./backgrounds/webp/handshake.webp?url";
+import webpBgMobHandshake from "./backgrounds/webp/mob-handshake.webp?url";
 
 const img = {
   iconCheck,
@@ -26,12 +32,12 @@ const img = {
   iconBungalowW,
   iconBungalowB,
   iconBungalowG,
-  backgroundsBoilerLeft,
-  backgroundsBoilerRight,
-  backgroundsHero,
-  backgroundsHandshake,
-  backgroundsMobHero,
-  backgroundsMobHandshake,
+  bgBoilerLeft,
+  bgBoilerRight,
+  bgHero,
+  bgHandshake,
+  bgMobHero,
+  bgMobHandshake,
   iconCombiW,
   iconCombiB,
   iconStandardW,
@@ -40,6 +46,12 @@ const img = {
   iconSystemB,
   iconBackBoilerW,
   iconBackBoilerB,
+  webpBgBoilerLeft,
+  webpBgBoilerRight,
+  webpBgHero,
+  webpBgHandshake,
+  webpBgMobHero,
+  webpBgMobHandshake,
 };
 
 export default img;
